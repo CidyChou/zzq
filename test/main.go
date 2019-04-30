34eef6f140265fe66dc87051afc9c645f6d6978c
20190046 <494608448@qq.com>
2019-04-30 16:43:41 +0800

    状态机demo

--- test/main.go
+++ test/main.go
@@ -2,73 +2,66 @@
 package main
 
 import (
+	"cidychou/enjoy-test/fsm"
 	"fmt"
 )
 
 var (
-	Attack = FSMState("攻击")
-	Move   = FSMState("移动")
-	Skill  = FSMState("释放技能")
+	Search = fsm.FSMState("寻找目标")
+	Attack = fsm.FSMState("攻击")
+	Move   = fsm.FSMState("移动")
+	Skill  = fsm.FSMState("释放技能")
 
-	AttackEvent = FSMEvent("攻击敌方英雄")
-	MoveEvent   = FSMEvent("向敌方英雄移动")
-	SkillEvent  = FSMEvent("释放技能")
+	TargetDealEvent    = fsm.FSMEvent("目标死亡")
+	TargetOutSideEvent = fsm.FSMEvent("目标在范围内")
+	TargetInSidEvent   = fsm.FSMEvent("目标在范围外")
 
-	AttackHandler = FSMHandler(func() FSMState {
-		fmt.Println("已攻击敌方英雄")
-		return Attack
+	TargetDealHandler = fsm.FSMHandler(func() fsm.FSMState {
+		fmt.Println("在移动时目标已死亡,重新搜寻目标")
+		return Search
 	})
-	MoveHandler = FSMHandler(func() FSMState {
-		fmt.Println("已向敌方英雄移动")
+
+	TargetOutSideHandler = fsm.FSMHandler(func() fsm.FSMState {
+		fmt.Println("敌人在攻击范围以外,移动")
 		return Move
 	})
-	SkillHandler = FSMHandler(func() FSMState {
-		fmt.Println("已释放技能")
-		return Skill
+
+	// TargetInSideHandler is 目标在攻击范围外
+	TargetInSideHandler = fsm.FSMHandler(func() fsm.FSMState {
+		fmt.Println("敌人在攻击范围以内,攻击")
+		return Attack
 	})
 )
 
 // 电风扇
 type ElectricFan struct {
-	*FSM
+	*fsm.FSM
 }
 
 // 实例化电风扇
-func NewElectricFan(initState FSMState) *ElectricFan {
+func NewElectricFan(initState fsm.FSMState) *ElectricFan {
 	return &ElectricFan{
-		FSM: NewFSM(initState),
+		FSM: fsm.NewFSM(initState),
 	}
 }
 
 // 入口函数
 func main() {
+	efan := NewElectricFan(Search) // 初始状态是关闭的
+	// 寻找目标
+	efan.AddHandler(Search, TargetOutSideEvent, TargetOutSideHandler)
+	efan.AddHandler(Search, TargetInSidEvent, TargetInSideHandler)
 
-	efan := NewElectricFan(Poweroff) // 初始状态是关闭的
-	// 关闭状态
-	efan.AddHandler(Poweroff, PowerOffEvent, PowerOffHandler)
-	efan.AddHandler(Poweroff, FirstGearEvent, FirstGearHandler)
-	efan.AddHandler(Poweroff, SecondGearEvent, SecondGearHandler)
-	efan.AddHandler(Poweroff, ThirdGearEvent, ThirdGearHandler)
-	// 1档状态
-	efan.AddHandler(FirstGear, PowerOffEvent, PowerOffHandler)
-	efan.AddHandler(FirstGear, FirstGearEvent, FirstGearHandler)
-	efan.AddHandler(FirstGear, SecondGearEvent, SecondGearHandler)
-	efan.AddHandler(FirstGear, ThirdGearEvent, ThirdGearHandler)
-	// 2档状态
-	efan.AddHandler(SecondGear, PowerOffEvent, PowerOffHandler)
-	efan.AddHandler(SecondGear, FirstGearEvent, FirstGearHandler)
-	efan.AddHandler(SecondGear, SecondGearEvent, SecondGearHandler)
-	efan.AddHandler(SecondGear, ThirdGearEvent, ThirdGearHandler)
-	// 3档状态
-	efan.AddHandler(ThirdGear, PowerOffEvent, PowerOffHandler)
-	efan.AddHandler(ThirdGear, FirstGearEvent, FirstGearHandler)
-	efan.AddHandler(ThirdGear, SecondGearEvent, SecondGearHandler)
-	efan.AddHandler(ThirdGear, ThirdGearEvent, ThirdGearHandler)
+	// 攻击状态
+	efan.AddHandler(Attack, TargetDealEvent, TargetDealHandler)
+
+	// 移动状态
+	efan.AddHandler(Move, TargetDealEvent, TargetDealHandler)
+	efan.AddHandler(Move, TargetInSidEvent, TargetInSideHandler)
 
 	// 开始测试状态变化
-	efan.Call(ThirdGearEvent)  // 按下3档按钮
-	efan.Call(FirstGearEvent)  // 按下1档按钮
-	efan.Call(PowerOffEvent)   // 按下关闭按钮
-	efan.Call(SecondGearEvent) // 按下2档按钮
-	efan.Call(PowerOffEvent)   // 按下关闭按钮
+	efan.Call(TargetOutSideEvent)
+	efan.Call(TargetInSidEvent)
+	efan.Call(TargetDealEvent)
+	efan.Call(TargetInSidEvent)
 }
